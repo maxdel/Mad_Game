4d84fa48a7932ae11d2ecd5eda0f1c473e95090b
alexanderguk <alex.guk.kpi@gmail.com>
2014-11-06 16:43:33 +0200

    Basic Structure

---
+++ src/core/Core.java
@@ -0,0 +1,25 @@
+package core;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Core extends BasicGame {
+
+    public Core(String gamename) {
+        super(gamename);
+    }
+
+    @Override
+    public void init(GameContainer gc) throws SlickException {}
+
+    @Override
+    public void update(GameContainer gc, int i) throws SlickException {}
+
+    @Override
+    public void render(GameContainer gc, Graphics g) throws SlickException {
+        g.drawString("Hello, Alexander!", 300, 200);
+    }
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
+import core.Core;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            AppGameContainer appgc;
+            appgc = new AppGameContainer(new Core("Simple Slick Game"));
+            appgc.setDisplayMode(640, 480, false);
+            appgc.start();
+        }
+        catch (SlickException ex) {
+            Logger.getLogger(Core.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
