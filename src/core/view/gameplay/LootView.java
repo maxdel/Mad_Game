4dbc132f677b48b67bd4c6b1c7cfafdcfeac60f4
Alexander Guk <alex.guk.kpi@gmail.com>
2014-11-17 13:32:12 +0200

    Added showing loot name for selected loot

--- src/core/view/gameplay/LootView.java
+++ src/core/view/gameplay/LootView.java
@@ -6,25 +6,48 @@ import core.model.gameplay.World;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
+
+import java.awt.Font;
 
 public class LootView {
 
     private Loot loot;
     private Image image;
+    TrueTypeFont ttf;
 
     protected LootView(Loot loot) {
         this.loot = loot;
         image = ResourceManager.getInstance().getItemImage(loot.getItem().getName());
+        Font font = new Font("sfd", Font.PLAIN, 10);
+        ttf = new TrueTypeFont(font, true);
     }
 
     public void render(Graphics g, double viewX, double viewY, float viewDegreeAngle, int viewWidth, int viewHeight)
             throws SlickException {
         rotate(g, viewX, viewY, viewDegreeAngle, viewWidth, viewHeight, true);
-        draw(viewX, viewY);
+        draw(viewX, viewY, g);
+
+        // ----- For debug and FUN -----
+        if (World.getInstance().getHero().getSelectedLoot() == loot) {
+            g.rotate((float) (loot.getX() - viewX),
+                    (float) (loot.getY() - viewY),
+                    (float) (viewDegreeAngle - loot.getDirection() / Math.PI * 180));
+            String text = String.valueOf(loot.getItem().getName());
+            if (loot.getNumber() > 1) {
+                text = text + "(" + String.valueOf(loot.getNumber()) + ")";
+            }
+            ttf.drawString((float) (loot.getX() - viewX - ttf.getWidth(text) / 2F),
+                    (float) (loot.getY() - viewY - ttf.getHeight(text) / 2F - 18), text);
+            g.rotate((float) (loot.getX() - viewX),
+                    (float) (loot.getY() - viewY),
+                    (float) (- viewDegreeAngle + loot.getDirection() / Math.PI * 180));
+        }
+        // ----- END -----
         rotate(g, viewX, viewY, viewDegreeAngle, viewWidth, viewHeight, false);
     }
 
-    public void rotate(Graphics g, final double viewX, final double viewY, final float viewDegreeAngle,
+    private void rotate(Graphics g, final double viewX, final double viewY, final float viewDegreeAngle,
                        final int viewWidth, final int viewHeight, final boolean isFront) {
         if (isFront) {
             g.rotate(viewWidth / 2, viewHeight / 2, - viewDegreeAngle);
@@ -39,13 +62,13 @@ public class LootView {
         }
     }
 
-    public void draw(final double viewX, final double viewY) {
+    private void draw(double viewX, double viewY, Graphics g) {
         if (World.getInstance().getHero().getSelectedLoot() == loot) {
-            ResourceManager.getInstance().getImage("Selected loot").draw((float) (loot.getX() - viewX - image.getWidth() / 2),
-                    (float) (loot.getY() - viewY - image.getHeight() / 2), 0.5F);
+            ResourceManager.getInstance().getImage("Selected loot").draw((float) (loot.getX() - viewX - image.getWidth() / 4),
+                    (float) (loot.getY() - viewY - image.getHeight() / 4), 0.5F);
         }
-        image.draw((float) (loot.getX() - viewX - image.getWidth() / 2),
-                (float) (loot.getY() - viewY - image.getHeight() / 2), 0.5F);
+        image.draw((float) (loot.getX() - viewX - image.getWidth() / 4),
+                (float) (loot.getY() - viewY - image.getHeight() / 4), 0.5F);
     }
 
     protected Loot getLoot() {
