7e080fdf6dedc8c89516ba7329edb83f2de6b254
Alexander Guk <alex.guk.kpi@gmail.com>
2014-12-12 14:21:13 +0200

    Refactoring

--- src/core/model/gameplay/items/LootRecord.java
+++ src/core/model/gameplay/items/LootRecord.java
@@ -3,15 +3,15 @@ package core.model.gameplay.items;
 public class LootRecord {
 
     private Item item;
-    private double probablility;
+    private double probability;
 
-    public LootRecord(Item item, double probablility) {
+    public LootRecord(Item item, double probability) {
         this.item = item;
-        this.probablility = probablility;
+        this.probability = probability;
     }
 
     public Loot generateLoot(double x, double y) {
-        if (Math.random() < probablility) {
+        if (Math.random() < probability) {
             return new Loot(x, y, item);
         } else {
             return null;
